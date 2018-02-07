a0f66fc0964ca8be67b20c7a151bfeab5d26bd86
abhinavsindhu <codenamex97@gmail.com>
2018-02-08 02:12:29 +0530

    adding countdown

--- GUESS THE RIGHT KEY.cpp
+++ GUESS THE RIGHT KEY.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
 #include<conio.h>
+#include<windows.h>
+
 using namespace std;
 int main() {
     int i,A[5]={21,36,67,90,34},t=5,x,key;
-
+srand(time(0));
     cout<<"CHOSE THE RIGHT KEY\n";
+    cout<<"Countdown timer";
     while(t--)
-    {
+    {   cout<<"\n\n"<<t;
+         _sleep(1);
         cout<<"\nTHE KEYS ARE \n";
         for(i=0;i<5;i++)
             cout<<A[i]<<" ";
         cout<<"\n";
      cin>>x;
-        srand(time(0));
+
         key=A[rand()%5];
         if(x==key)
             cout<<"\n YOU HAVE CHOSEN THE RIGHT KEY";
